cca54b9e55cfa57ce605a04a2ee5c4dce64fe90f
Sourabh <sourabhhowale@gmail.com>
2026-05-20 13:13:55 +0530

    changes of starture

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+//
+
+let accountEmail = "Gd7s2@example.com";
+
+var accountPassword = "mySuperSecretPassword";
+
+accountCity = "New York";
+
+
+accountId =2;
+
+console.log(accountId);
