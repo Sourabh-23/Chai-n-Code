c933eba26113517459df09528d1113673585ed06
Sourabh <sourabhhowale@gmail.com>
2026-05-20 20:09:30 +0530

    datatype

---
+++ 01_basics/02 _datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3); // 6
+// we are using node js not browser so alert is not defined
+
+console.log(3+3); // 6
+
+console.log("Hello World");
+
+let name = "John Doe";
+
+let age = 23;
+
+let isLoggedIn = false;
+
+// Primitives => number, bigint, string, boolean, null, undefined, symbol
+
+// number => 2 to power 53
+// bigint => more than 2 to power 53
+//string => " ", ' ', ` `
+//boolean => true or false
+//null => standalone value
+//undefined => undefined
+//symbol => unique identifier
+
+console.log("type of name =",typeof name);
+console.log("type of age =",typeof age);
+console.log("type of isLoggedIn =",typeof isLoggedIn);
+console.log("type of null =",typeof null);
+console.log("type of undefined =",typeof undefined);
+
+// symbol => unique identifier
