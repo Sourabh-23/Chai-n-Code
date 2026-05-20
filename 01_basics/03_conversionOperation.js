4c34154b3308a3262721c8856400da69dfdf5d54
Sourabh <sourabhhowale@gmail.com>
2026-05-20 20:33:27 +0530

    Datatyope conversion confusion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+ let score ="33";
+//let score = "33abc";
+//let score =true;
+//let score = null;
+//let score = undefined;
+//let score = Symbol("33");
+
+//let score = "33abc";
+
+
+
+
+console.log("score =",typeof score);
+
+let valueInNumber = Number(score);
+
+console.log("valueInNumber =",typeof valueInNumber);
+
+console.log("valueInNumber =",valueInNumber);
+
+
+let booleanScore = Boolean(score);
+
+console.log("booleanScore =",typeof booleanScore);
+
+let isloggedIn = "sourabh";
+let booleanIsloggedIn = Boolean(isloggedIn);
+console.log("booleanIsloggedIn =",booleanIsloggedIn);
+
+let emptyString = "";
+let booleanEmptyString = Boolean(emptyString);
+console.log("booleanEmptyString =",booleanEmptyString);
