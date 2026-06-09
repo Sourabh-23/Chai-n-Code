357d2bff2205cb9faf552777d77812e5a8b0a23e
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-06-09 13:55:25 +0530

    do while

---
+++ 5.loops__iterartions/while & do while loop.js
@@ -0,0 +1,36 @@
+// while loop
+
+
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//  i=i+2;
+// }
+
+
+
+
+// let Array = ["flash", "batman", "superman", "wonderwoman"];
+
+// let i=0;
+
+// while (i < Array.length) {
+//     console.log(Array[i]);
+//     i++;
+// }
+
+
+let i = 0;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+// Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
