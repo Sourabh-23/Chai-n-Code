7ca0c98c7de55435098d158bf2b1df1701eff1f2
Sourabh <112743236+Sourabh-23@users.noreply.github.com>
2026-06-11 12:32:06 +0530

    .

--- 5.loops__iterartions/10_7.js
+++ 5.loops__iterartions/10_7.js
@@ -4,15 +4,20 @@ const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     return num + 10;
 // });
 
+
 const newnums = mynums.map((num) => num + 10);
+
 
 console.log(newnums);
 // [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-// Chaining of methods
-const total = mynums
-    .filter((num) => num > 5)
-    .map((num) => num + 10);
-    //.reduce((sum, num) => sum + num, 0);
 
-console.log(total);
+
+// chaining of methods
+
+const newnums = mynums
+    .filter((num) => num > 5)
+    .map((num) => num + 10).
+    reduce((sum, num) => sum + num, 0);
+
+console.log(newnums);
